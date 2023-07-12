649fa0beddcbceee3e0a35b0a260c1ade79bc53b
eunhye0619 <eunhye3814@gmail.com>
2023-07-12 13:42:21 +0900

    일정 추가

--- Planner.js
+++ Planner.js
@@ -40,3 +40,35 @@ const makeCalendar = (date) => {
     document.querySelector(`.nextDay`).onclick = () => {
     makeCalendar(new Date(date.setMonth(date.getMonth() + 1)));
 }
+
+//일정 추가
+function addEvent() {
+  var choDate = document.getElementById('cho_date').value;
+  var eventInput = document.getElementById('eventInput');
+  var event = eventInput.value.trim();
+
+  if (choDate === '') {
+    alert('날짜를 선택해주세요.');
+    return;
+  }
+
+  if (event === '') {
+    alert('일정을 입력해주세요.');
+    return;
+  }
+
+  var dateItems = document.querySelectorAll('.dateBoard > div');
+
+  for(var i=0; i<dateItems.length;i++){
+    var dateItem = dateItems[i];
+    if(dateItem.innerText === choDate){
+      var eventItem = document.createElement('p');
+      eventItem.innerText=event;
+      dateItem.appendChild(eventItem);
+      return;
+    }
+  }
+
+  eventInput.value='';
+  document.getElementById('cho_date').value='';
+}
